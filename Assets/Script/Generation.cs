241ce294f593b4fd25ed966e193c3d2e7d2000c5
Reborn77310 <44340480+Reborn77310@users.noreply.github.com>
2018-10-23 17:44:00 +0200

    début 3eme scene
    tir des ia avec deplacements
    manque :
    -deplacement avatar
    -competences
    -tourelles

--- Assets/Script/Generation.cs
+++ Assets/Script/Generation.cs
@@ -6,103 +6,125 @@ using UnityEngine.UI;
 
 public class Agents
 {
-	public GameObject go;
-	public NavMeshAgent nma;
+    public GameObject go;
+    public NavMeshAgent nma;
 
-	public Agents(GameObject go_)
-	{
-		go = go_;
-	}
+    public Agents(GameObject go_)
+    {
+        go = go_;
+    }
 
-	public void NMA(NavMeshAgent nma_)
-	{
-		nma = nma_;
-	}
+    public void NMA(NavMeshAgent nma_)
+    {
+        nma = nma_;
+    }
 }
 
-public class Generation : MonoBehaviour {
+public class Generation : MonoBehaviour
+{
 
-	List<Agents> allAgents = new List<Agents>();
-	public int nbAgentToGenerate;
-	float timerMax = 5;
-	float timer = 0;
-	public Color agtColor;
-	public GameObject sol;
-	Color color_1 = new Color(0,0.5f,1,1);
-	Color color_2 = new Color(0.5f,1,0,1);
-	Color color_3 = new Color(1,0,0.5f,1);
-	public static float AgentsAngleColor = 0;
-	public Text t;
+    List<Agents> allAgents = new List<Agents>();
+    public int nbAgentToGenerate;
+    float timerMax = 5;
+    float timer = 0;
+    public Color agtColor;
+    public GameObject sol;
+    Color color_1 = new Color(0, 0.5f, 1, 1);
+    Color color_2 = new Color(0.5f, 1, 0, 1);
+    Color color_3 = new Color(1, 0, 0.5f, 1);
+    public static float AgentsAngleColor = 0;
+    public Text t;
+	[SerializeField] GameObject bullet;
+    public int radius;
+    void Start()
+    {
+        InitGeneration();
+    }
 
-	void Start () {
-		InitGeneration();
-	}
+    void Update()
+    {
+        //ChangeAgentsColor();
+        shoot();
+    }
 
-	void Update() {
-		ChangeAgentsColor();
-	}
+    void InitGeneration()
+    {
+        for (int i = 0; i < nbAgentToGenerate; i++)
+        {
+            Agents newAgent = new Agents(Instantiate(Resources.Load("Prefab/Agent_") as GameObject));
+            newAgent.NMA(newAgent.go.GetComponent<NavMeshAgent>());
+            newAgent.go.name = "Agent_" + i.ToString();
+            newAgent.go.transform.position = RandomNavmeshLocation(radius);
+            allAgents.Add(newAgent);
+        }
+    }
 
-	void InitGeneration()
-	{
-		for (int i = 0; i < nbAgentToGenerate; i++)
-		{
-			Agents newAgent = new Agents(Instantiate(Resources.Load("Prefab/Agent_") as GameObject));
-			newAgent.NMA(newAgent.go.GetComponent<NavMeshAgent>());
-			newAgent.go.name = "Agent_" + i.ToString();
-			newAgent.go.transform.position = RandomNavmeshLocation(8);
-			allAgents.Add(newAgent);
-		}
-	}
-	
-	Vector3 RandomNavmeshLocation(float radius) {
-         Vector3 randomDirection = Random.insideUnitSphere * radius;
-         randomDirection += transform.position;
-         NavMeshHit hit;
-         Vector3 finalPosition = Vector3.zero;
-         if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
-             finalPosition = hit.position;            
-         }
-         return finalPosition;
-     }
+    Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
 
-	 void ChangeAgentsColor()
-	{
+    void shoot()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timerMax)
+        {
+            for (int i = 0; i < allAgents.Count; i++)
+            {
+                GameObject balle = Instantiate(bullet,allAgents[i].go.transform.position,Quaternion.identity);
+				balle.GetComponent<Rigidbody>().AddForce((GameObject.Find("Player").transform.position - allAgents[i].go.transform.position).normalized * 5, ForceMode.VelocityChange);
+            }
+            timer = 0;
+        }
+    }
 
-		timer += Time.deltaTime;
-		if (timer >= timerMax)
-		{
-			Color nextColor =  ColorSelectionCalculation();
-			for (int i =0; i <allAgents.Count; i++)
-			{
-				allAgents[i].go.GetComponent<MeshRenderer>().material.color = nextColor;
-			}
-			//sol.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value,Random.value, 0.4f);
-			timer = 0;
-		}
-	}
+    void ChangeAgentsColor()
+    {
 
-	Color ColorSelectionCalculation()
-	{	
-		float x = Random.Range(-360,0);
-		AgentsAngleColor = x;
-		Color actualColor;
-		if (x >= -120)
-		{
-			var z = Mathf.Abs(x) / 120;
-			actualColor = Color.Lerp(color_1, color_2, z);
-		}
-		else if (x < -120 && x >= -240)
-		{
-			var y = x + 120;
-			var z = Mathf.Abs(y) / 120;
-			actualColor = Color.Lerp(color_2, color_3, z);
-		}
-		else{
-			var y = x + 240;
-			var z = Mathf.Abs(y) / 120;
-			actualColor = Color.Lerp(color_3, color_1, z);
-		}
-		return actualColor;
-	}
+        timer += Time.deltaTime;
+        if (timer >= timerMax)
+        {
+            Color nextColor = ColorSelectionCalculation();
+            for (int i = 0; i < allAgents.Count; i++)
+            {
+                allAgents[i].go.GetComponent<MeshRenderer>().material.color = nextColor;
+            }
+            //sol.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value,Random.value, 0.4f);
+            timer = 0;
+        }
+    }
+
+    Color ColorSelectionCalculation()
+    {
+        float x = Random.Range(-360, 0);
+        AgentsAngleColor = x;
+        Color actualColor;
+        if (x >= -120)
+        {
+            var z = Mathf.Abs(x) / 120;
+            actualColor = Color.Lerp(color_1, color_2, z);
+        }
+        else if (x < -120 && x >= -240)
+        {
+            var y = x + 120;
+            var z = Mathf.Abs(y) / 120;
+            actualColor = Color.Lerp(color_2, color_3, z);
+        }
+        else
+        {
+            var y = x + 240;
+            var z = Mathf.Abs(y) / 120;
+            actualColor = Color.Lerp(color_3, color_1, z);
+        }
+        return actualColor;
+    }
 
 }
